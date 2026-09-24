4a7af36c329eb26cab324a3e952005f849b94cdf
Carlos Humberto <camehudev@gmail.com>
2026-09-24 10:58:32 -0300

    alterado URL

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -9,6 +9,7 @@ export class CameraService {
 
   // Substitua pela URL correta da sua API (ou IP da sua VPS/Easypanel)
   //private apiUrl = 'http://localhost:8000/stock/scan-image';
+  private apiUrl2 = 'https://pessoal-marju-express.sjj3wv.easypanel.host/stock';
 
   constructor(private http: HttpClient) {}
 
@@ -16,6 +17,6 @@ export class CameraService {
     const formData = new FormData();
     formData.append('file', file, file.name);
     
-    return this.http.post<any>('https://pessoal-marju-express.sjj3wv.easypanel.host',formData);
+    return this.http.post<any>(`${this.apiUrl2}/scan-image`,formData);
   }
 }
