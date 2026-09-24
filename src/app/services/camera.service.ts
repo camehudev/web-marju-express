2fcef9568a119730af6b04e7ea4f1ab9dcff5ebc
Carlos Humberto <camehudev@gmail.com>
2026-09-24 10:07:46 -0300

    Ajuste URL API

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs/internal/Observable';
 export class CameraService {
 
   // Substitua pela URL correta da sua API (ou IP da sua VPS/Easypanel)
-  private apiUrl = 'http://localhost:8000/stock/scan-image';
+  //private apiUrl = 'http://localhost:8000/stock/scan-image';
 
   constructor(private http: HttpClient) {}
 
@@ -16,6 +16,6 @@ export class CameraService {
     const formData = new FormData();
     formData.append('file', file, file.name);
     
-    return this.http.post<any>('http://localhost:8000/stock/scan-image',formData);
+    return this.http.post<any>('https://pessoal-marju-express.sjj3wv.easypanel.host',formData);
   }
 }
