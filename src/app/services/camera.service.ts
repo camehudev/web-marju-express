a649ccd2d6b0ab1cb14f6478bee816d71f2c6018
Carlos Humberto <camehudev@gmail.com>
2026-09-24 21:56:57 -0300

    Ajuste URL production 2

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -9,7 +9,7 @@ export class CameraService {
   // URL da sua API FastAPI
   private apiUrl = 'http://127.0.0.1:8000/stock/scan-image'; // Ou o link de produção da sua VPS
 
-  private apiBase = 'https://pessoal-web-marju-express.sjj3wv.easypanel.host/stock/scan-image'
+  private apiBase = 'https://pessoal-marju-express.sjj3wv.easypanel.host/stock/scan-image'
 
   constructor(private http: HttpClient) {}
 
@@ -17,6 +17,6 @@ export class CameraService {
     const formData = new FormData();
     formData.append('file', file, file.name);
 
-    return this.http.post<any>(this.apiBase, formData);
+    return this.http.post<any>(`${this.apiBase}`, formData);
   }
 }
